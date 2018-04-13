975a046683becf8f2e838fa25ed8c07b4eabaa0f
guimin <guimin.lin@baozun.com>
2018-04-13 14:24:47 +0800

    modify the reference path for the file.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import CreateCampaign from '@/pages/createCampaign'
+import CreateCampaign from '@/pages/createCampaign/Index'
 
 Vue.use(Router)
 
