473d33dd09d5de7ba5ca9d03af8976904cbba56c
guimin <guimin.lin@baozun.com>
2018-04-18 09:56:23 +0800

    Remove redundant code.

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import CreateCampaign from '@/pages/createCampaign/Index'
 import EditCampaign from '@/pages/editCampaign/Index'
-import BrowseCampaign from '@/pages/browseCampaign/Index'
 
 Vue.use(Router)
 
@@ -16,10 +15,6 @@ export default new Router({
       path: '/edit-campaign/:id',
       name: 'EditCampaign',
       component: EditCampaign
-    }, {
-      path: '/browse-campaign/:id',
-      name: 'BrowseCampaign',
-      component: BrowseCampaign
     }
   ]
 })
